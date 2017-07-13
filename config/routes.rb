b3b29b8464e7ec9e772ba2e60f6dd8c919e33171
helena ostengen <helenarql@gmail.com>
2017-07-13 13:07:56 +0000

    add index

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   resources :photos
   resources :posts
   
+  root 'photos#index'
+  
   root 'posts#index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
